ece5abc1bd433f8781ca1d87e7b21dc0ed72b7b3
Petryk <57829569+Sasha-hk@users.noreply.github.com>
2021-08-14 06:27:31 -0500

    fix naming on init.sh

--- init.sh
+++ init.sh
@@ -26,18 +26,18 @@ pip install -r env/requirements.txt
 sed -i "s%domain_name_replace%$domain_name%g" env/nginx/project1
 sed -i "s%proxy_pass_replace%$proxy_pass%g" env/nginx/project1  
 
-mv env/nginx/project1 env/nginx/project_name
+mv env/nginx/project1 env/nginx/$project_name
 
 sed -i "s%base_dir_replace%$base_dir%g" env/gunicorn/project1.service env/gunicorn/project1.socket
 sed -i "s%project_name_replace%$project_name%g" env/gunicorn/project1.service env/gunicorn/project1.socket
 sed -i "s%user_replace%$user%g" env/gunicorn/project1.service
 
-mv env/gunicorn/project1.service env/gunicorn/project_name.service
-mv env/gunicorn/project1.socket env/gunicorn/project_name.socket
+mv env/gunicorn/project1.service env/gunicorn/$project_name.service
+mv env/gunicorn/project1.socket env/gunicorn/$project_name.socket
 
 sed -i "s%domain_name_replace%$domain_name%g" src/config/settings.py
 
-ln -s $base_dir/env/gunicorn/project.service /etc/systemd/system 
-ln -s $base_dir/env/gunicorn/project.socket /etc/systemd/system 
+ln -s $base_dir/env/gunicorn/$project_name.service /etc/systemd/system 
+ln -s $base_dir/env/gunicorn/$project_name.socket /etc/systemd/system 
 
-ln -s $base_dir/env/nginx/project /etc/nginx/sites-enabled
+ln -s $base_dir/env/nginx/$project_name /etc/nginx/sites-enabled

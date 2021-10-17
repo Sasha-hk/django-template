c505421bb9ff590a11257e7beecb63e839c75d87
Sasha-hk <sachapos50@gmail.com>
2021-10-17 17:17:27 +0300

    Moved init.sh to the root

--- install.sh
+++ install.sh
@@ -12,46 +12,51 @@ domain_name=""
 project_name=""
 user_to_run_gunicorn=""
 gunicorn_workers_count=3 
-base_dir="$(dirname "$PWD")"
+base_dir="$PWD"
 
+echo "${RED}[!]${None} You must have installed:
+        - nginx
+        - gunicotn\n"
+ 
 
 # Get neded variables
 
-echo -n "${CYAN}[add]${None} Enter domain name, for example 'google.com': "
+echo -n "${CYAN}[+]${None} Enter domain name, for example 'google.com': "
 read domain_name 
 
-echo -n "${CYAN}[add]${None} Enter project name, for example 'google': "
+echo -n "${CYAN}[+]${None} Enter project name, for example 'google': "
 read project_name
 
-echo -n "${CYAN}[add]${None} Enter user name, to run gunicorn: "
+echo -n "${CYAN}[+]${None} Enter user name, to run gunicorn: "
 read user_to_run_gunicorn
 
-echo -n "${CYAN}[add]${None} Enter count of workers for gunicorn: "
+echo -n "${CYAN}[+]${None} Enter count of workers for gunicorn: "
 read gunicorn_workers_count 
 
 
 
-# Set bot start file
-sed -i "s%replace%$base_dir%g" $base_dir/env/start_bot.sh
+# replace domain name on setting.py
+
+sed -i "s/domain_name_replace/${domain_name}/g" src/config/settings.py
 
 
 
 # Create and activate virtualenviroment
 
+cd env
 virtualenv env
-# source env/bin/activate
 . env/bin/activate
 pip install -r requirements.txt
 
 
 
-# Create and fill servir files
+# Create and fill server files
 
 mkdir nginx systemd
 mkdir -p systemd/log
 
 path_to_sock_file="/run/$project_name.sock"
- 
+
 
 
 ## NGINX
@@ -92,6 +97,8 @@ WantedBy=multi-user.target" >> systemd/$project_name.service
 
 
 # Creating a symbolic link for server files
+echo "${RED}[!]${None} Now we need to set some files which need root access, enter root password"
+su root
 
 su -c "ln -s $PWD/nginx/$project_name /etc/nginx/sites-enabled"
 
--- src/config/settings.py
+++ src/config/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
